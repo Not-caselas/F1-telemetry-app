428449658ae5448f3675bb9313059a4c3e6dc306
Not-caselas <cosras@outlook.com>
2023-04-12 11:30:39 +0200

    Update Formula1datav0.12.py

--- Formula1datav0.12.py
+++ Formula1datav0.12.py
@@ -37,6 +37,11 @@ DriverLabel2=ct.CTkLabel(win, text="Driver 2 ID:")
 Driver4=StringVar()
 DriverEntry2=ct.CTkEntry(win, textvariable=Driver4)
 
+value = ct.StringVar(value="Distance")
+
+Switch = ct.CTkSwitch(master=win, text="Use time for the x axis instead of distance",
+variable=value, onvalue="Time", offvalue="Distance")
+
 YearLabel.grid(row=0,column=0)
 YearEntry.grid(row=0,column=1)
 RaceLabel.grid(row=1,column=0)
@@ -47,145 +52,159 @@ DriverLabel.grid(row=3,column=0)
 DriverEntry.grid(row=3,column=1)
 DriverLabel2.grid(row=4,column=0)
 DriverEntry2.grid(row=4,column=1)
+Switch.grid(row=5, columnspan=2)
 
 def comparison():
-    global yearTK
-    global raceTK
-    global SessionTK
-    global Driver1
-    global Driver2
-    year=yearTK.get()
-    race=raceTK.get()
-    Session=SessionTK.get()
-    Driver1=Driver3.get()
-    Driver2=Driver4.get()
+    try:
+        global yearTK
+        global raceTK
+        global SessionTK
+        global Driver1
+        global Driver2
+        global value
+        year=yearTK.get()
+        race=raceTK.get()
+        Session=SessionTK.get()
+        Driver1=Driver3.get()
+        Driver2=Driver4.get()
+        Value=value.get()
 
-    quali = ff1.get_session(year, race, Session)
+        Driver1=Driver1.upper()
+        Driver2=Driver2.upper()
+
+        quali = ff1.get_session(year, race, Session)
+        
+        laps=quali.load_laps(with_telemetry=True)
+        lapsDriver1=laps.pick_driver(Driver1)
+        lapsDriver2=laps.pick_driver(Driver2)
+
+        fastest_driver_1 = lapsDriver1.pick_fastest()
+        fastest_driver_2 = lapsDriver2.pick_fastest()
+        telemetry_driver_1 = fastest_driver_1.get_telemetry().add_distance()
+        telemetry_driver_2 = fastest_driver_2.get_telemetry().add_distance()
+        delta_time, ref_tel, compare_tel = utils.delta_time(fastest_driver_1, fastest_driver_2)
+
+        plot_size = [15, 15]
+        plot_ratios = [1, 3, 2, 1, 1, 2, 1]
+
+        # Create subplots with different sizes
+        fig, ax = plt.subplots(7, gridspec_kw={'height_ratios': plot_ratios})
+        fig.patch.set_facecolor('xkcd:midnight blue')
+        # Set the plot title
+        ax[0].set_title('Fastest lap comparison'+" "+race+" "+str(year)+" "+Driver1+" "+"VS"+" "+Driver2, color='white')
+
+
+        # Delta line
+        ax[0].plot(ref_tel[Value], delta_time)
+        ax[0].axhline(0)
+        ax[0].set_ylabel(str("Gap to"+" "+Driver2+"(s)"), color="white")
+
+        ax[0].tick_params(axis='x', which="both", colors='white')
+        ax[0].tick_params(axis='y', which="both", colors='white')
+        ax[0].spines['left'].set_color('white')
+        ax[0].spines['bottom'].set_color('white')
+        ax[0].spines['right'].set_color('white')
+        ax[0].spines['top'].set_color('white')
+        ax[0].set_facecolor('xkcd:midnight blue')
+
+        # Speed trace
+        ax[1].plot(telemetry_driver_1[Value], telemetry_driver_1['Speed'], label=Driver1+" "+str(fastest_driver_1["LapTime"]).replace("0 days 00:0", "").replace("000", ""), color=ff1.plotting.driver_color(Driver1))
+        ax[1].plot(telemetry_driver_2[Value], telemetry_driver_2['Speed'], label=Driver2+" "+str(fastest_driver_2["LapTime"]).replace("0 days 00:0", "").replace("000", ""), color=ff1.plotting.driver_color(Driver2))
+        ax[1].set_ylabel('speed', color='white')
+        ax[1].legend(loc="lower right")
+
+        ax[1].tick_params(axis='x', which="both", colors='white')
+        ax[1].tick_params(axis='y', which="both", colors='white')
+        ax[1].spines['left'].set_color('white')
+        ax[1].spines['bottom'].set_color('white')
+        ax[1].spines['right'].set_color('white')
+        ax[1].spines['top'].set_color('white')
+        ax[1].set_facecolor('xkcd:midnight blue')
+
+
+        # Throttle trace
+        ax[2].plot(telemetry_driver_1[Value], telemetry_driver_1['Throttle'], label=Driver1, color=ff1.plotting.driver_color(Driver1))
+        ax[2].plot(telemetry_driver_2[Value], telemetry_driver_2['Throttle'], label=Driver2, color=ff1.plotting.driver_color(Driver2))
+        ax[2].set_ylabel('Throttle', color="white")
+
+        ax[2].tick_params(axis='x', which="both", colors='white')
+        ax[2].tick_params(axis='y', which="both", colors='white')
+        ax[2].spines['left'].set_color('white')
+        ax[2].spines['bottom'].set_color('white')
+        ax[2].spines['right'].set_color('white')
+        ax[2].spines['top'].set_color('white')
+        ax[2].set_facecolor('xkcd:midnight blue')
+
+
+        # Brake trace
+        ax[3].plot(telemetry_driver_1[Value], telemetry_driver_1['Brake'], label=Driver1, color=ff1.plotting.driver_color(Driver1))
+        ax[3].plot(telemetry_driver_2[Value], telemetry_driver_2['Brake'], label=Driver2, color=ff1.plotting.driver_color(Driver2))
+        ax[3].set_ylabel('Brake', color="white")
+
+        ax[3].tick_params(axis='x', which="both", colors='white')
+        ax[3].tick_params(axis='y', which="both", colors='white')
+        ax[3].spines['left'].set_color('white')
+        ax[3].spines['bottom'].set_color('white')
+        ax[3].spines['right'].set_color('white')
+        ax[3].spines['top'].set_color('white')
+        ax[3].set_facecolor('xkcd:midnight blue')
+
+        # Gear trace
+        ax[4].plot(telemetry_driver_1[Value], telemetry_driver_1['nGear'], label=Driver1, color=ff1.plotting.driver_color(Driver1))
+        ax[4].plot(telemetry_driver_2[Value], telemetry_driver_2['nGear'], label=Driver2, color=ff1.plotting.driver_color(Driver2))
+        ax[4].set_ylabel('Gear', color="white")
+
+        ax[4].tick_params(axis='x', which="both", colors='white')
+        ax[4].tick_params(axis='y', which="both", colors='white')
+        ax[4].spines['left'].set_color('white')
+        ax[4].spines['bottom'].set_color('white')
+        ax[4].spines['right'].set_color('white')
+        ax[4].spines['top'].set_color('white')
+        ax[4].set_facecolor('xkcd:midnight blue')
+
+        # RPM trace
+        ax[5].plot(telemetry_driver_1[Value], telemetry_driver_1['RPM'], label=Driver1, color=ff1.plotting.driver_color(Driver1))
+        ax[5].plot(telemetry_driver_2[Value], telemetry_driver_2['RPM'], label=Driver2, color=ff1.plotting.driver_color(Driver2))
+        ax[5].set_ylabel('RPM', color="white")
+
+        ax[5].tick_params(axis='x', which="both", colors='white')
+        ax[5].tick_params(axis='y', which="both", colors='white')
+        ax[5].spines['left'].set_color('white')
+        ax[5].spines['bottom'].set_color('white')
+        ax[5].spines['right'].set_color('white')
+        ax[5].spines['top'].set_color('white')
+        ax[5].set_facecolor('xkcd:midnight blue')
+
+        if Value=="Distance":
+            unit=" (meters)"
+            valueunit="distance"
+        else:
+            unit=""
+            valueunit="time"
+
+        # DRS trace
+        ax[6].plot(telemetry_driver_1[Value], telemetry_driver_1['DRS'], label=Driver1, color=ff1.plotting.driver_color(Driver1))
+        ax[6].plot(telemetry_driver_2[Value], telemetry_driver_2['DRS'], label=Driver2, color=ff1.plotting.driver_color(Driver2))
+        ax[6].set_ylabel('DRS', color="white")
+        ax[6].set_xlabel('Lap'+ " " + valueunit + unit, color="white")
+
+        ax[6].tick_params(axis='x', which="both", colors='white')
+        ax[6].tick_params(axis='y', which="both", colors='white')
+        ax[6].spines['left'].set_color('white')
+        ax[6].spines['bottom'].set_color('white')
+        ax[6].spines['right'].set_color('white')
+        ax[6].spines['top'].set_color('white')
+        ax[6].set_facecolor('xkcd:midnight blue')
+
+
+        # Hide x labels and tick labels for top plots and y ticks for right plots.
+        for a in ax.flat:
+            a.label_outer()
+        plt.show() 
     
-    laps=quali.load_laps(with_telemetry=True)
-    lapsDriver1=laps.pick_driver(Driver1)
-    lapsDriver2=laps.pick_driver(Driver2)
-
-    fastest_driver_1 = lapsDriver1.pick_fastest()
-    fastest_driver_2 = lapsDriver2.pick_fastest()
-    telemetry_driver_1 = fastest_driver_1.get_telemetry().add_distance()
-    telemetry_driver_2 = fastest_driver_2.get_telemetry().add_distance()
-    delta_time, ref_tel, compare_tel = utils.delta_time(fastest_driver_1, fastest_driver_2)
-
-    plot_size = [15, 15]
-    plot_ratios = [1, 3, 2, 1, 1, 2, 1]
-
-    # Create subplots with different sizes
-    fig, ax = plt.subplots(7, gridspec_kw={'height_ratios': plot_ratios})
-    fig.patch.set_facecolor('xkcd:midnight blue')
-    # Set the plot title
-    ax[0].set_title('Fastest lap comparison'+" "+race+" "+str(year)+" "+Driver1+" "+"VS"+" "+Driver2, color='white')
-
-
-    # Delta line
-    ax[0].plot(ref_tel['Distance'], delta_time)
-    ax[0].axhline(0)
-    ax[0].set_ylabel(str("Gap to"+" "+Driver2+"(s)"), color="white")
-
-    ax[0].tick_params(axis='x', which="both", colors='white')
-    ax[0].tick_params(axis='y', which="both", colors='white')
-    ax[0].spines['left'].set_color('white')
-    ax[0].spines['bottom'].set_color('white')
-    ax[0].spines['right'].set_color('white')
-    ax[0].spines['top'].set_color('white')
-    ax[0].set_facecolor('xkcd:midnight blue')
-
-    # Speed trace
-    ax[1].plot(telemetry_driver_1['Distance'], telemetry_driver_1['Speed'], label=Driver1+" "+str(fastest_driver_1["LapTime"]).replace("0 days 00:0", "").replace("000", ""), color=ff1.plotting.driver_color(Driver1))
-    ax[1].plot(telemetry_driver_2['Distance'], telemetry_driver_2['Speed'], label=Driver2+" "+str(fastest_driver_2["LapTime"]).replace("0 days 00:0", "").replace("000", ""), color=ff1.plotting.driver_color(Driver2))
-    ax[1].set_ylabel('speed', color='white')
-    ax[1].legend(loc="lower right")
-
-    ax[1].tick_params(axis='x', which="both", colors='white')
-    ax[1].tick_params(axis='y', which="both", colors='white')
-    ax[1].spines['left'].set_color('white')
-    ax[1].spines['bottom'].set_color('white')
-    ax[1].spines['right'].set_color('white')
-    ax[1].spines['top'].set_color('white')
-    ax[1].set_facecolor('xkcd:midnight blue')
-
-
-    # Throttle trace
-    ax[2].plot(telemetry_driver_1['Distance'], telemetry_driver_1['Throttle'], label=Driver1, color=ff1.plotting.driver_color(Driver1))
-    ax[2].plot(telemetry_driver_2['Distance'], telemetry_driver_2['Throttle'], label=Driver2, color=ff1.plotting.driver_color(Driver2))
-    ax[2].set_ylabel('Throttle', color="white")
-
-    ax[2].tick_params(axis='x', which="both", colors='white')
-    ax[2].tick_params(axis='y', which="both", colors='white')
-    ax[2].spines['left'].set_color('white')
-    ax[2].spines['bottom'].set_color('white')
-    ax[2].spines['right'].set_color('white')
-    ax[2].spines['top'].set_color('white')
-    ax[2].set_facecolor('xkcd:midnight blue')
-
-
-    # Brake trace
-    ax[3].plot(telemetry_driver_1['Distance'], telemetry_driver_1['Brake'], label=Driver1, color=ff1.plotting.driver_color(Driver1))
-    ax[3].plot(telemetry_driver_2['Distance'], telemetry_driver_2['Brake'], label=Driver2, color=ff1.plotting.driver_color(Driver2))
-    ax[3].set_ylabel('Brake', color="white")
-
-    ax[3].tick_params(axis='x', which="both", colors='white')
-    ax[3].tick_params(axis='y', which="both", colors='white')
-    ax[3].spines['left'].set_color('white')
-    ax[3].spines['bottom'].set_color('white')
-    ax[3].spines['right'].set_color('white')
-    ax[3].spines['top'].set_color('white')
-    ax[3].set_facecolor('xkcd:midnight blue')
-
-    # Gear trace
-    ax[4].plot(telemetry_driver_1['Distance'], telemetry_driver_1['nGear'], label=Driver1, color=ff1.plotting.driver_color(Driver1))
-    ax[4].plot(telemetry_driver_2['Distance'], telemetry_driver_2['nGear'], label=Driver2, color=ff1.plotting.driver_color(Driver2))
-    ax[4].set_ylabel('Gear', color="white")
-
-    ax[4].tick_params(axis='x', which="both", colors='white')
-    ax[4].tick_params(axis='y', which="both", colors='white')
-    ax[4].spines['left'].set_color('white')
-    ax[4].spines['bottom'].set_color('white')
-    ax[4].spines['right'].set_color('white')
-    ax[4].spines['top'].set_color('white')
-    ax[4].set_facecolor('xkcd:midnight blue')
-
-    # RPM trace
-    ax[5].plot(telemetry_driver_1['Distance'], telemetry_driver_1['RPM'], label=Driver1, color=ff1.plotting.driver_color(Driver1))
-    ax[5].plot(telemetry_driver_2['Distance'], telemetry_driver_2['RPM'], label=Driver2, color=ff1.plotting.driver_color(Driver2))
-    ax[5].set_ylabel('RPM', color="white")
-
-    ax[5].tick_params(axis='x', which="both", colors='white')
-    ax[5].tick_params(axis='y', which="both", colors='white')
-    ax[5].spines['left'].set_color('white')
-    ax[5].spines['bottom'].set_color('white')
-    ax[5].spines['right'].set_color('white')
-    ax[5].spines['top'].set_color('white')
-    ax[5].set_facecolor('xkcd:midnight blue')
-
-    # DRS trace
-    ax[6].plot(telemetry_driver_1['Distance'], telemetry_driver_1['DRS'], label=Driver1, color=ff1.plotting.driver_color(Driver1))
-    ax[6].plot(telemetry_driver_2['Distance'], telemetry_driver_2['DRS'], label=Driver2, color=ff1.plotting.driver_color(Driver2))
-    ax[6].set_ylabel('DRS', color="white")
-    ax[6].set_xlabel('Lap distance (meters)', color="white")
-
-    ax[6].tick_params(axis='x', which="both", colors='white')
-    ax[6].tick_params(axis='y', which="both", colors='white')
-    ax[6].spines['left'].set_color('white')
-    ax[6].spines['bottom'].set_color('white')
-    ax[6].spines['right'].set_color('white')
-    ax[6].spines['top'].set_color('white')
-    ax[6].set_facecolor('xkcd:midnight blue')
-
-
-    # Hide x labels and tick labels for top plots and y ticks for right plots.
-    for a in ax.flat:
-        a.label_outer()
-    plt.show() 
-    
-    #except:
-        #fail=Label(win, text="You did something wrong, restart the program please")
-        #fail.grid(row=7, column=0)
+    except:
+        fail=Label(win, text="You did something wrong, restart the program please")
+        fail.grid(row=7, column=0)
 
 
 
@@ -211,8 +230,9 @@ def function():
 
 
 confirmButton=ct.CTkButton(win, text="Confirm", command=comparison)
-confirmButton.grid(row=5,columnspan=2)
+confirmButton.grid(row=6,columnspan=2)
 
+win.bind("enter", confirmButton)
 
 win.mainloop()
 
